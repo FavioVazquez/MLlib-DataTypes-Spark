5215993817e47230fbd9c078e4dfb368cdf13b70
FavioVazquez <favio.vazquezp@gmail.com>
2015-05-15 11:22:56 -0430

    - Started local matrix section

--- src/main/scala/DataTypesMLlib.scala
+++ src/main/scala/DataTypesMLlib.scala
@@ -18,21 +18,21 @@ def main(args: Array[String]) {
   val sc = new SparkContext(conf)
 
   /** MLlib supports local vectors and matrices stored on a single machine,
-  * as well as distributed matrices backed by one or more RDDs. Local vectors
-  * and local matrices are simple data models that serve as public interfaces.
-  * The underlying linear algebra operations are provided by Breeze and jblas.
-  */
+    * as well as distributed matrices backed by one or more RDDs. Local vectors
+    * and local matrices are simple data models that serve as public interfaces.
+    * The underlying linear algebra operations are provided by Breeze and jblas.
+    */
 
-//  1. Local Vector
+  //  1. Local Vector
 
- /** A local vector has integer-typed and 0-based indices and double-typed
-  * values, stored on a single machine. MLlib supports two types of local
-  * vectors: dense and sparse. A dense vector is backed by a double array
-  * representing its entry values, while a sparse vector is backed by two
-  * parallel arrays: indices and values. For example, a vector (1.0, 0.0, 3.0)
-  * can be represented in dense format as [1.0, 0.0, 3.0] or in sparse format
-  * as (3, [0, 2], [1.0, 3.0]), where 3 is the size of the vector.
-  */
+  /** A local vector has integer-typed and 0-based indices and double-typed
+    * values, stored on a single machine. MLlib supports two types of local
+    * vectors: dense and sparse. A dense vector is backed by a double array
+    * representing its entry values, while a sparse vector is backed by two
+    * parallel arrays: indices and values. For example, a vector (1.0, 0.0, 3.0)
+    * can be represented in dense format as [1.0, 0.0, 3.0] or in sparse format
+    * as (3, [0, 2], [1.0, 3.0]), where 3 is the size of the vector.
+    */
 
   /**
    * The base class of local vectors is Vector, and we provide two
@@ -40,13 +40,13 @@ def main(args: Array[String]) {
    * the factory methods implemented in Vectors to create local vectors.
    */
 
-// DenseVector(values: Array[Double])
+  // DenseVector(values: Array[Double])
 
   // Create a dense vector (1.0, 0.0, 3.0).
   val dv: Vector = Vectors.dense(1.0, 0.0, 3.0)
   println(dv)
 
-//  SparseVector(size: Int, indices: Array[Int], values: Array[Double])
+  //  SparseVector(size: Int, indices: Array[Int], values: Array[Double])
 
   // Create a sparse vector (1.0, 0.0, 3.0) by specifying its indices and values corresponding to nonzero entries.
   val sv1: Vector = Vectors.sparse(3, Array(0, 2), Array(1.0, 3.0))
@@ -57,7 +57,7 @@ def main(args: Array[String]) {
 
 
 
-//  2. Labeled Point
+  //  2. Labeled Point
 
   /**
    * A labeled point is a local vector, either dense or sparse, associated
@@ -69,7 +69,7 @@ def main(args: Array[String]) {
    * from zero: 0, 1, 2, ....
    */
 
-//  LabeledPoint(label: Double, features: Vector)
+  //  LabeledPoint(label: Double, features: Vector)
 
   // Create a labeled point with a positive label and a dense feature vector.
   val pos = LabeledPoint(1.0, Vectors.dense(1.0, 0.0, 3.0))
@@ -94,11 +94,20 @@ def main(args: Array[String]) {
    * the feature indices are converted to zero-based.
    */
 
-//  MLUtils.loadLibSVMFile reads training examples stored in LIBSVM format.
-
+  //  MLUtils.loadLibSVMFile reads training examples stored in LIBSVM format.
 
   val examples: RDD[LabeledPoint] = MLUtils.loadLibSVMFile(sc, "/opt/spark/data/mllib/sample_libsvm_data.txt")
   println(examples)
+
+  //  3. Local Matrix
+
+  /**
+   * A local matrix has integer-typed row and column indices and double-typed
+   * values, stored on a single machine. MLlib supports dense matrices, whose
+   * entry values are stored in a single double array in column major.
+   */
+
+  
 
   sc.stop()
   }
