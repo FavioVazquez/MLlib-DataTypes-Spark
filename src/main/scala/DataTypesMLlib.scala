eceb5adf12be129cbf5df59c163039b248da593d
FavioVazquez <favio.vazquezp@gmail.com>
2015-05-15 11:06:18 -0430

    - Finished Labeled Points section

--- src/main/scala/DataTypesMLlib.scala
+++ src/main/scala/DataTypesMLlib.scala
@@ -1,5 +1,7 @@
 import org.apache.spark.mllib.linalg.{Vector, Vectors}
 import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.util.MLUtils
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkContext, SparkConf}
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
@@ -80,6 +82,27 @@ def main(args: Array[String]) {
   // Create a labeled point with a negative label and a sparse feature vector.
   val neg = LabeledPoint(0.0, Vectors.sparse(3, Array(0, 2), Array(1.0, 3.0)))
   println(neg)
+
+  /**
+   * Sparse data
+   *
+   * It is very common in practice to have sparse training data. MLlib
+   * supports reading training examples stored in LIBSVM format, which
+   * is the default format used by LIBSVM and LIBLINEAR. It is a text
+   * format in which each line represents a labeled sparse feature vector
+   * using the following format:
+   *
+   * label index1:value1 index2:value2 ...
+   *
+   * where the indices are one-based and in ascending order. After loading,
+   * the feature indices are converted to zero-based.
+   */
+
+//  MLUtils.loadLibSVMFile reads training examples stored in LIBSVM format.
+
+
+  val examples: RDD[LabeledPoint] = MLUtils.loadLibSVMFile(sc, "/opt/spark/data/mllib/sample_libsvm_data.txt")
+  println(examples)
 
   sc.stop()
   }
