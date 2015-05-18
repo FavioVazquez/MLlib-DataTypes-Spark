8c1232f65181e52cd04b920ca8e070ef836b61f6
FavioVazquez <favio.vazquezp@gmail.com>
2015-05-18 10:32:25 -0430

    - Try with 10g

--- src/main/scala/DataTypesMLlib.scala
+++ src/main/scala/DataTypesMLlib.scala
@@ -17,7 +17,7 @@ def main(args: Array[String]) {
 //  .setMaster("local")
     .setMaster("mesos://master.mcbo.mood.com.ve:5050")
     .setAppName("Data Types MLlib")
-    .set("spark.executor.memory", "6g")
+    .set("spark.executor.memory", "10g")
   val sc = new SparkContext(conf)
 
   /** MLlib supports local vectors and matrices stored on a single machine,
